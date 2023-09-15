1adfa16ac737c833c0915d5d264e02c3e523f10c
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-09-15 18:02:15 +0300

    Responsive design left

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
                   <img class="rounded-t-lg" src="./icons/proje1.png" alt="" />
                 </a>
                 <div class="p-5">
-                  <a href="https://vercel.com/dogan-keles/redux-filmler">
+                  <a href="https://redux-filmler-zeta.vercel.app/movies">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                       {text.projects.pItems[0].title}
                     </h5>
@@ -25,7 +25,7 @@ function Projects() {
                     {text.projects.pItems[0].descr}
                   </p>
                   <a
-                    href="https://vercel.com/dogan-keles/redux-filmler"
+                    href="https://redux-filmler-zeta.vercel.app/movies"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     {text.projects.pItems[0].goToLink}
